d225207aa74e994d446c6d7dbfc316e470ca3f6e
ayrtonbric95 <iqbriceno@gmail.com>
2024-06-12 21:43:26 -0400

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Ayrton Briceno on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
